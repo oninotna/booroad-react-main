84f4d957b4fa893b5f8116cb954921c4fbdcc749
oninotna <antoninomusarratubbi@gmail.com>
2025-11-10 12:21:10 +0100

    fixes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,12 +4,14 @@ import { Link } from "react-router-dom";
 export default function HomePage() {
   return (
     <div className="container">
-      <div className="d-flex justify-content-between align-items-center my-3">
-        
-        <img className="home-img" src="../public/img/iconBooroad.png" alt="" />
-        <h1 className="my-4">Viaggi</h1>
+      
+      <div className="d-flex justify-content-between align-items-center my-3">    
+        <img className="home-img" src="/img/iconBooroad.png" alt="" />
+        <h1 className="my-4 home-title">Viaggi</h1>
         <Link to="/travels/newtravel" className="btn btn-primary">Aggiungi nuovo viaggio</Link>
       </div>
+      <div className="text-center">
+        <h1 className="my-4 home-title-mobile">Viaggi</h1></div>
       <TravelList />
     </div>
   );
